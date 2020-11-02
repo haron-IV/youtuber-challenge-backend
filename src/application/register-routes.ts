96bb4119fda65c7ba4ca14b8cce8451135ed9976
Bartek <bartek.bober.koder@gmail.com>
2020-11-02 17:58:02 +0100

    preventing save same user

--- src/application/register-routes.ts
+++ src/application/register-routes.ts
@@ -1,9 +1,7 @@
 import { Application } from 'express'
-import { testRouter } from '../routes/test-route'
 import { signupRouter } from '../routes/signup-route'
 
 const registerAppRoutes = (app: Application): void => {
-  app.use('/test', testRouter)
   app.use('/signup', signupRouter)
 }
 
