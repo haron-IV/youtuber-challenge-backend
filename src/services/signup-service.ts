1cdcaf3a8350ca2886f65fbfbbb0703b10e438fb
Bartek <bartek.bober.koder@gmail.com>
2020-11-11 16:45:35 +0100

    refactorize signup service

--- src/services/signup-service.ts
+++ src/services/signup-service.ts
@@ -23,7 +23,7 @@ const saveUser = async (data: saveSignupInreface) => {
   const { email, hashedPassword, res } = data
 
   if (!await checkIfEmailExist(email)) {
-    const confirmationCode = generateConfirmationCode()
+    const confirmationCode: String = generateConfirmationCode()
     const user = new User({
       email,
       password: hashedPassword,
@@ -31,8 +31,7 @@ const saveUser = async (data: saveSignupInreface) => {
       confirmationCode
     })
   
-    await user
-    .save()
+    await user.save()
     .then( (): void => {
       logger.info(`User ${email} signed up.`)
       sendMail({
@@ -40,36 +39,35 @@ const saveUser = async (data: saveSignupInreface) => {
         subject: 'Youtuber challenge account verification',
         message: `Hello. Next step is veryfication account. Retype this code in application: ${confirmationCode}`
       })
-      res.status(201).json({
-        message: 'User signed up.'
-      })
-    }).catch((err): void => {
-      res.status(500).json({
-        error: err
-      })
+      res.status(201).json({message: 'User signed up.'})
+    })
+    .catch((err): void => {
+      res.status(500).json({ error: err })
     })
   } else {
-    res.status(500).json({ error: "Email was used already" })
+    res.status(500).json({ error: "Email in use." })
   }
 }
 
-const checkIfUsernameExist = async (username: String) => {
+const checkIfUsernameExist = async (username: String): Promise<Boolean> => {
   return await User.find({ username })
   .then((user): Boolean => {
     if (user.length !== 0) return true
     return false
-  }).catch((err): Boolean => { 
+  })
+  .catch((err): Boolean => { 
     logger.error(err)
     return false
   })
 }
 
-const checkIfEmailExist = async (email: string) => {
+const checkIfEmailExist = async (email: string): Promise<Boolean> => {
   return await User.find({email})
   .then((user): Boolean => {
     if(user.length !== 0) return true
     return false
-  }).catch((err): Boolean => {
+  })
+  .catch((err): Boolean => {
     logger.error(err)
     return false
   })
@@ -79,24 +77,19 @@ const signupUser = async (signupUser: signupUserInreface) => {
   const { email, password, res } = signupUser
 
   hash(password, 10, async (err, hashedPassword) => {
-    if(err){
-      return res.status(500).json({
-        error: err
-      })
-    } else {
-      saveUser({ email, hashedPassword, res })
-    }
+    if(err) return res.status(500).json({ error: err })
+    else saveUser({ email, hashedPassword, res })
   })
 }
 
-const setUsername = async (email: string, username: string) => {
+const setUsername = async (email: string, username: string): Promise<Response | Object | undefined> => {
   if (await checkIfUsernameIsAllowed(username)) {
-    await User.update({email}, {username}).then(res => {
+    await User.update({email}, {username})
+    .then(res => {
       logger.info('Username settled.')
       res.status(201).json(true)
-    }).catch(err => { 
-      logger.error(err) 
     })
+    .catch(err => { logger.error(err)} )
   } else {
     logger.info(`Cannot set this username: ${username}`)
     return new Promise((resolve) => {
@@ -105,21 +98,18 @@ const setUsername = async (email: string, username: string) => {
   }
 }
 
-const getVerificationCode = async (email:string | undefined) => await User.find({ email }, 'confirmationCode')
+const getVerificationCode = async (email: string | undefined) => await User.find({ email }, 'confirmationCode')
 
 const checkIfUsernameExistOnNotAllowedUsernamesList = (username: string): boolean => {
   const usernames = notAllowedUsernames
-  if(usernames.indexOf(username) !== -1) {
-    return false
-  }
+  if(usernames.indexOf(username) !== -1) return false
+
   logger.info(`${username} cannot be used cuz it is on not allowed nicknames list`)
   return true
 }
 
-const checkIfUsernameIsAllowed = async (username: string) => {
-  if (!await checkIfUsernameExist(username) && checkIfUsernameExistOnNotAllowedUsernamesList(username)) {
-    return true
-  }
+const checkIfUsernameIsAllowed = async (username: string): Promise<boolean> => {
+  if (!await checkIfUsernameExist(username) && checkIfUsernameExistOnNotAllowedUsernamesList(username)) return true
   return false
 }
 
